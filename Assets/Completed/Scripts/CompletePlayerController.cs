d2f78897ae89acd3c782d3305e504bd8f447f811
bagelSeed <mark.luo05@gmail.com>
2016-05-12 16:29:11 -0400

    Respawn added

--- Assets/Completed/Scripts/CompletePlayerController.cs
+++ Assets/Completed/Scripts/CompletePlayerController.cs
@@ -8,42 +8,39 @@ using UnityEngine.UI;
 [RequireComponent(typeof(LineRenderer))]
 public class CompletePlayerController : MonoBehaviour {
 
-	public float speed;				//Floating point variable to store the player's movement speed.
-    public float friction;				//Floating point variable to store the player's movement speed.
-	public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
+	public float speed;				
+    public float friction;
+    public float percentage;        
+
+    public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
 	public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
 
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
 	private int count;				//Integer to store the number of pickups collected so far.
 
     LineRenderer lineRenderer;
-    public float threshold = 0.001f;
     int lineCount = 0;
 
     public Camera camera;
 
     Vector2 direction;
 
-    void Awake()
-    {
+    // Use this for initialization
+    void Start()
+	{
         camera = Camera.main;
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.sortingLayerName = "Player";
         lineRenderer.sortingOrder = 1;
-    }
-
-    // Use this for initialization
-    void Start()
-	{
-		//Get and store a reference to the Rigidbody2D component so that we can access it.
-		rb2d = GetComponent<Rigidbody2D> ();
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D> ();
 		//Initialize count to zero.
 		count = 0;
 		//Initialze winText to a blank string since we haven't won yet at beginning.
 		winText.text = "";
 		//Call our SetCountText function which will update the text with the current value for count.
 		SetCountText ();
-        
+        spawnPlayer();
     }
 
     void OnMouseDrag()
@@ -51,7 +48,6 @@ public class CompletePlayerController : MonoBehaviour {
         lineRenderer.SetVertexCount(2);
 
         direction = transform.position - camera.ScreenToWorldPoint(Input.mousePosition);
-        //Vector3 static_Direction = new Vector3(transform.position.x + 10, transform.position.y + 10, transform.position.z);
 
         if (direction.magnitude > 7)
         { 
@@ -65,7 +61,7 @@ public class CompletePlayerController : MonoBehaviour {
     void OnMouseUp()
     {
         // World pos - world pos
-        Vector2 movement = direction * speed * 100;
+        Vector2 movement = direction * speed * percentage;
         rb2d.AddForce(movement);
         lineRenderer.SetVertexCount(0);
     }
@@ -96,6 +92,10 @@ public class CompletePlayerController : MonoBehaviour {
 			//Update the currently displayed count by calling the SetCountText function.
 			SetCountText ();
 		}
+        else if (other.gameObject.CompareTag("Background"))
+        {
+            killPlayer();
+        }
 	}
 
 	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
@@ -108,4 +108,25 @@ public class CompletePlayerController : MonoBehaviour {
 			//... then set the text property of our winText object to "You win!"
 			winText.text = "You win!";
 	}
+
+    void killPlayer()
+    {
+        // Maybe reset a bunch of crap here:
+        // Fade player and then spawn if possible
+        shrinkPlayer();
+    }
+
+    void shrinkPlayer()
+    {
+        spawnPlayer();
+    }
+
+    void spawnPlayer()
+    {
+        // Eventually get some kind of init position getting for each map
+        Vector2 initialPosition = new Vector2(0, 0);
+        Vector2 initialVelocity = new Vector2(0, 0);
+        transform.position = initialPosition;
+        rb2d.velocity = initialVelocity;
+    }
 }
